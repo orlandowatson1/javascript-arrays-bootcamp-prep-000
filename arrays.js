80ba46da59e6a38075a82b896cf91dfbc15c0215
Orlando Watson <orlando.watson@journalism.cuny.edu>
2018-07-17 17:11:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,6 @@ function addElementToBeginningOfArray (chocolateBars,pizza) {
   return [pizza,...chocolateBars];
 }
 function destructivelyAddElementToBeginningOfArray (chocolateBars,pizza) {
-  return [chocolateBars, pizza];
   chocolateBars.unshift (pizza);
-  return
+  return [..., pizza];
 }
