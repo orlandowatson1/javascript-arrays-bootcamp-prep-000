01f123bf5360ff630ad1de179b2b27ed88e6b935
Orlando Watson <orlando.watson@journalism.cuny.edu>
2018-07-17 17:09:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,3 +2,6 @@ var chocolateBars = ["snickers" , "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray (chocolateBars,pizza) {
   return [pizza,...chocolateBars];
 }
+function destructivelyAddElementToBeginningOfArray (chocolateBars,pizza) {
+  return [,];
+}
