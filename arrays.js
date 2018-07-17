c028ca0b72d6d600fd8c445b110311c7f75e4825
Orlando Watson <orlando.watson@journalism.cuny.edu>
2018-07-17 17:12:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,3 +6,12 @@ function destructivelyAddElementToBeginningOfArray (chocolateBars,pizza) {
   chocolateBars.unshift (pizza);
   return chocolateBars;
 }
+function addElementToEndOfArray(chocolateBars, pizza){
+  return [...chocolateBars, pizza];
+}
+ 
+function destructivelyAddElementToEndOfArray(chocolateBars, pizza){
+  chocolateBars.push(pizza);
+  return chocolateBars;
+}
+ 
